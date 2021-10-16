aabfd38f6b20f7d1d752081771a0045a9b4d8500
hamzadunbok <92648459+hamzadunbok@users.noreply.github.com>
2021-10-16 22:44:52 +0300

    Update hamza.js

--- hamza.js
+++ hamza.js
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>hallo jo</h2>
+
+<p id="demo"></p>
+
+<script>
+const cars = ["marcedes", "ferarri", "BMW"];
+document.getElementById("demo").innerHTML = cars;
+</script>
+
+</body>
+</html>
